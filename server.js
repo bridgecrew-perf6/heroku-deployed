fb4b7cec5b31cd626fae7b44ba7ba283247df6f1
pueert1324 <pueert1324@gmail.com>
2020-12-18 15:07:40 +0700

    'assas'

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// app.set('port', process.env.PORT || 3000);
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/RecordRTCProject'));
+
+// app.get('/*', function(req,res) {
+
+// res.sendFile(path.join(__dirname+'/dist/RecordRTCProject/index.html'));
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 3000,function() {
+//   console.log('localhost:' + app.get('port'));
+// });
+
 //Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/RecordRTCProject'));
+app.use(express.static(__dirname + '/dist/myapp'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/RecordRTCProject/index.html'));
+res.sendFile(path.join(__dirname+'/dist/myapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000,function() {
-  console.log('localhost:' + app.get('port'));
+
+app.listen(process.env.PORT || 8080,function() {
+  console.log('localhost:' + app.get('port'))
 });
